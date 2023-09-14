5b97df574347e8de9b78fa5dfbb3475add58c239
GiancarloAlvarez <galvarez@infosyp.com>
2023-09-14 12:14:10 -0400

    anothe rupdate

--- EblueWorkPlan/EblueWorkPlan/Controllers/HomeController.cs
+++ EblueWorkPlan/EblueWorkPlan/Controllers/HomeController.cs
@@ -2,13 +2,17 @@
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using EblueWorkPlan.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Components.Routing;
 
 namespace EblueWorkPlan.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
+        private List<User> _UserItems;
+        private readonly WorkplandbContext _context;
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -40,12 +44,46 @@ namespace EblueWorkPlan.Controllers
         }
 
         [HttpPost]
-        public IActionResult Signin(User user) 
+        public IActionResult Signin(User user, UserViewModel _User)
         {
-            UserViewModel userViewModel= new UserViewModel();
+
+           
+            //List<User> users = new List<User>();
+            //List<User> validateuser = new List<User>();
+            //foreach (var item in users)
+            //{
+                
 
 
-            return View();
+            //}
+
+
+            //User ValidateUser(string _email, string _password)
+            //{
+
+            //     user.Where(item => item.Email == _email && item.Password == _password).FirstOrDefault();
+
+
+
+
+
+            //}
+
+            //var _user = ValidateUser(user.Email, user.Password);
+
+            if (_User.Email == user.Email && _User.Password == user.Password)
+            {
+
+
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+
+                return View();
+
+            }
+
         }
     }
 }
--- EblueWorkPlan/EblueWorkPlan/Models/ViewModels/UserViewModel.cs
+++ EblueWorkPlan/EblueWorkPlan/Models/ViewModels/UserViewModel.cs
@@ -1,24 +1,16 @@
-﻿namespace EblueWorkPlan.Models.ViewModels
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EblueWorkPlan.Models.ViewModels
 {
     public class UserViewModel
     {
-        public List<User> UserList() { 
-            
-            return new List<User>();
-        
-        
-        }
+        public int userId { get; set; }
+
+        public string Email { get; set; }
+        public string Password { get; set; }
 
 
 
-        public User ValidateUser(string _email, string _password) {
-
-            return UserList().Where(item => item.Email == _email && item.Password == _password).FirstOrDefault();
-        
-        
-        
-        
-        
-        }
+       
     }
 }
